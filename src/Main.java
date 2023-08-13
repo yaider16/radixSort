e1e4999d7d9eca045b86e67308bc035bfe88114d
Yaider <yaideralejo2@gmail.com>
2023-08-13 09:46:46 -0500

    Despues de la ayuda del github copilot finalmente funcionando perfectamente el código, trataré de entenderlo completamente y ver si podria hacerlo por mi mismo

--- src/Main.java
+++ src/Main.java
@@ -34,103 +34,46 @@ public class Main {
         // radix sort algorithm
         int[] numsCopy = new int[MAX_NUMBERS];
         System.arraycopy(nums, 0, numsCopy, 0, nums.length);
+        int[] sortedArr = new int[MAX_NUMBERS];
+        int[] count = new int[10];
+        int[] temp = new int[MAX_NUMBERS];
+        int digit = 1;
+        int radix = 10;
+        int max = numsCopy[0];
+        int min = numsCopy[0];
 
-        int[][] sortedNums = new int[10][100];
 
-        //This for loop is to get the first digit of each number and put it in the array sortedNums
-        for (int i =0; i<MAX_NUMBERS;i++) {
-
-            // Here I get the first digit of the number
-            int digit  = Integer.parseInt(String.valueOf(String.valueOf(numsCopy[i]).charAt(0)));
-
-            // This for loop is to find an empty space in the array to put the number
-            for (int j = 0; j < 100; j++) {
-                if (sortedNums[digit][j] == 0) {
-                    sortedNums[digit][j] = numsCopy[i];
-                    break;
-                }
-            }
+        for (int i = 1; i < MAX_NUMBERS; i++) {
+            if (numsCopy[i] > max)
+                max = numsCopy[i];
+            if (numsCopy[i] < min)
+                min = numsCopy[i];
         }
 
+        int maxDigit = (int) Math.log10(max);
+        int minDigit = (int) Math.log10(min);
+        int maxDigits = Math.max(maxDigit, minDigit);
 
-        // This for loop is to get the second digit of each number and put it in the array sortedNums
-        int[][] sortedNums2 = new int[10][100];
-
-        for (int i =0; i<10;i++) {
-
-            // This for loop is to find an empty space in the array to put the number
-            for (int j = 0; j < 100; j++) {
-
-                // Here I get the second digit of the number and if it does not have a number I will count it as 0
-                int digit =0;
-                if (sortedNums[i][j]>9){
-                    digit = Integer.parseInt(String.valueOf(String.valueOf(numsCopy[i]).charAt(1)));
-                }
-
-                // Here I put the number in the array
-                if (sortedNums2[digit][j] == 0) {
-                    sortedNums2[digit][j] = sortedNums[i][j];
-                    break;
-                }
+        for (int i = 0; i < maxDigits + 1; i++) {
+            for (int j = 0; j < radix; j++) {
+                count[j] = 0;
             }
-        }
-
-        // This for loop is to get the third digit of each number and put it in the array sortedNums
-
-        int[][] sortedNums3 = new int[10][100];
-
-        for (int i =0; i<10;i++) {
-
-            // This for loop is to find an empty space in the array to put the number
-            for (int j = 0; j < 100; j++) {
-
-                // Here I get the third digit of the number and if it does not have a number I will count it as 0
-                int digit =0;
-                if (sortedNums2[i][j]>99){
-                    digit = Integer.parseInt(String.valueOf(String.valueOf(numsCopy[i]).charAt(2)));
-                }
-
-                // Here I put the number in the array
-                if (sortedNums3[digit][j] == 0) {
-                    sortedNums3[digit][j] = sortedNums2[i][j];
-                    break;
-                }
+            for (int j = 0; j < MAX_NUMBERS; j++) {
+                int subArr = (numsCopy[j] / digit) % radix;
+                count[subArr]++;
             }
-        }
-
-        // This for loop is to get the fourth digit of each number and put it in the array sortedNums
-
-        int[][] sortedNums4 = new int[10][100];
-
-        for (int i =0; i<10;i++) {
-
-            // This for loop is to find an empty space in the array to put the number
-            for (int j = 0; j < 100; j++) {
-
-                // Here I get the fourth digit of the number and if it does not have a number I will count it as 0
-                int digit =0;
-                if (sortedNums3[i][j]>999){
-                    digit = Integer.parseInt(String.valueOf(String.valueOf(numsCopy[i]).charAt(3)));
-                }
-
-                // Here I put the number in the array
-                if (sortedNums4[digit][j] == 0) {
-                    sortedNums4[digit][j] = sortedNums3[i][j];
-                    break;
-                }
+            for (int j = 1; j < radix; j++) {
+                count[j] += count[j - 1];
             }
-        }
-
-        // This for loop is to put everything into a one dimensional array
-        int count =0;
-        for (int i =0; i<10;i++) {
-            for (int j = 0; j < 100; j++) {
-                if (sortedNums4[i][j] != 0) {
-                    numsCopy[count] = sortedNums4[i][j];
-                    count++;
-                }
+            for (int j = MAX_NUMBERS - 1; j >= 0; j--) {
+                int subArr = (numsCopy[j] / digit) % radix;
+                temp[count[subArr] - 1] = numsCopy[j];
+                count[subArr]--;
             }
+            System.arraycopy(temp, 0, numsCopy, 0, MAX_NUMBERS);
+            digit *= 10;
         }
-        return numsCopy;
+        System.arraycopy(numsCopy, 0, sortedArr, 0, MAX_NUMBERS);
+        return sortedArr;
     }
 }
